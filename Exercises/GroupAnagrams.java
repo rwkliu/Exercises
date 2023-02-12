4c619f70324a4fa388778ab645fdf4cfda9f1ecc
rwkliu <114320485+rwkliu@users.noreply.github.com>
2023-02-12 04:17:34 -0800

    GroupAnagrams.java
    Optimized solution

--- Exercises/GroupAnagrams.java
+++ Exercises/GroupAnagrams.java
@@ -1,66 +1,44 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.HashMap;
 
-class GroupAnagrams {
-  public static void main(String[] args) {
-    String[] strs1 = {"eat","tea","tan","ate","nat","bat"};
-    String[] strs2 = {""};
-    String[] strs3 = {"a"};
-    System.out.println(groupAnagrams(strs1));
-    System.out.println(groupAnagrams(strs2));
-    System.out.println(groupAnagrams(strs3));
-  }
+class GroupAnagrams {                                                                               
+  public static void main(String[] args) {                                                          
+    String[] strs1 = {"eat","tea","tan","ate","nat","bat"};                                         
+    String[] strs2 = {""};                                                                          
+    String[] strs3 = {"a"};                                                                         
+    System.out.println(groupAnagrams(strs1));                                                       
+    System.out.println(groupAnagrams(strs2));                                                       
+    System.out.println(groupAnagrams(strs3));                                                       
+  }                                                                                                 
 
-  public static boolean isEqualStrings(char[] str1, char[] str2) {
-    if (str1.length != str2.length) {
-      return false;
-    }
-    for (int i = 0; i < str1.length; i++) {
-      if (str1[i] != str2[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  public static boolean isAnagram(String subListElement, String str) {
-    char[] sortedSubListElement = subListElement.toCharArray();
-    Arrays.sort(sortedSubListElement);
-    char[] sortedStr = str.toCharArray();
-    Arrays.sort(sortedStr);
-
-    if (isEqualStrings(sortedSubListElement, sortedStr)) { 
-      return true;
-    }
-    return false;
-  }
-
+  //Maintain a map where each key is a sorted string and its values are a list of words in str
+  //that are anagrams of the key                                                                    
   public static List<List<String>> groupAnagrams(String[] strs) {
     List<List<String>> output = new ArrayList<>();
-    List<String> firstList = new ArrayList<>(Arrays.asList(strs[0]));
-    output.add(firstList);
-    
     if (strs.length == 0) {
       return output;
     }
-
-    for (int i = 1; i < strs.length; i++) {
-      String element = new String(strs[i]);
-      boolean addFlag = false;
-
-      for (int j = 0; j < output.size(); j++) {
-        List<String> subList = new ArrayList<>(output.get(j));
-        if (isAnagram(subList.get(0), element)) {
-          output.get(j).add(element);
-          addFlag = true;
-        }
+    Map<String, List<String>> anagrams = new HashMap<>();
+   //Go through each string in the input array and check if the string is anagram of any keys in the 
+   //hash map. Add string to key. Otherwise, add current string's sorted string and current string
+   //as a new key,value pair to the hash map
+    for(String str : strs) {
+      char[] sortedStr = str.toCharArray();
+      Arrays.sort(sortedStr);
+      String key = String.valueOf(sortedStr);
+      if (anagrams.containsKey(key) == false) {
+        List<String> newsubList = new ArrayList<>();
+        anagrams.put(key, newsubList);
       }
-      if (addFlag == false) {
-        List<String> newSubList = new ArrayList<>(Arrays.asList(element));
-        output.add(newSubList);
-      }
+      anagrams.get(key).add(str);
     }
+    for (Map.Entry<String, List<String>> anagramList : anagrams.entrySet()) {
+      output.add(anagramList.getValue());
+    }
+
     return output;
   }
-}
+} 
