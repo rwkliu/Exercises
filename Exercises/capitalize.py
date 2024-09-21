92020728fa03dd3260085435c0d1b59529324f6d
rwkliu <114320485+rwkliu@users.noreply.github.com>
2024-09-21 00:48:30 -0700

    Revise capitalize.py with one liner

--- Exercises/capitalize.py
+++ Exercises/capitalize.py
@@ -2,25 +2,7 @@ def capitalize(param_1):
     if param_1 == "":
         return ""
 
-    param_1 = param_1.lower()
-    param_list = param_1.split(" ")
-
-    for i in range(len(param_list)):
-        word_list = list(param_list[i])
-        for j in range(len(word_list)):
-            if (
-                ord(word_list[j]) >= ord("a")
-                and ord(word_list[j]) <= ord("z")
-                or ord(word_list[j]) >= ord("A")
-                and ord(word_list[j]) <= ord("Z")
-            ):
-                word_list[j] = word_list[j].upper()
-                break
-            else:
-                break
-        param_list[i] = "".join(word_list)
-
-    return " ".join(param_list)
+    return " ".join([word.capitalize() for word in param_1.split()])
 
 
 in1 = "a FiRSt LiTTlE TESt"
